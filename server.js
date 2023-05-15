db008fe80e03b874df61aa8a51fdd952b80f702e
Jono Padoa <jonopadoa@gmail.com>
2023-05-16 09:54:19 +1200

    Built server.post endpoint /carValue to check API is working. Confirmed it is working.

--- server.js
+++ server.js
@@ -7,6 +7,23 @@ import express from "express";
 const server = express();
 server.use(express.json());
 
+server.post("/carValue", (req, res) => {
+  const { model, year } = req.body;
+
+  if (!model || !year) {
+    return res.status(400).json({ error: "Model and year are required." });
+  }
+
+  const modelValue = model
+    .toUpperCase()
+    .split("")
+    .reduce((acc, letter) => acc + (letter.charCodeAt(0) - 64) * 100, 0);
+
+  const carValue = modelValue + parseInt(year);
+
+  res.json({ carValue });
+});
+
 const PORT = 4000;
 server.listen(4000, () => {
   console.log(`server started on port 4000`);
