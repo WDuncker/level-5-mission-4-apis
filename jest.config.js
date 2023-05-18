65904c0b63b996487857ce270bac0fbde9abd720
Wilhelmus Duncker <wimduncker17@hotmail.co.nz>
2023-05-18 16:21:54 +1200

    committing pre pull

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
+("use strict");
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+};
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: "ts-jest",
