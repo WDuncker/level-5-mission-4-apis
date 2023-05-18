8ac29f7c0f5c9c13681f7d2500219b0981663eaf
Wilhelmus Duncker <wimduncker17@hotmail.co.nz>
2023-05-18 14:00:41 +1200

    final commit

--- build/services/calcService.js
+++ build/services/calcService.js
@@ -1,20 +1,21 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.monthlyPremium = exports.yearlyPremium = void 0;
-let yearly;
-let monthly;
+//Defining variables globally so they can accessed by either function
+let yearlyPrem;
+let monthlyPrem;
 const yearlyPremium = (carValue, riskRating) => {
     //This if statement makes sure that invalid numbers cannot be submitted
     if (!(carValue > 0 && riskRating > 0)) {
         throw new Error("Car value and risk rating must be greater than 0");
     }
-    yearly = (carValue * riskRating) / 100;
-    return yearly;
+    yearlyPrem = (carValue * riskRating) / 100;
+    return yearlyPrem;
 };
 exports.yearlyPremium = yearlyPremium;
 const monthlyPremium = (yearly) => {
     //Use of math object to get two decimal places as per normal dollar value conventions
-    monthly = Math.round((yearly / 12) * 100) / 100;
-    return monthly;
+    monthlyPrem = Math.round((yearly / 12) * 100) / 100;
+    return monthlyPrem;
 };
 exports.monthlyPremium = monthlyPremium;
